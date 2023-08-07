8a420c87889a2429fcd70e0ea2ed4dad439e18e9
MatheusScaranello <matheusscaranello.564@gmail.com>
2023-08-07 14:21:44 -0300

    feat: showpost in html

--- assets/js/sript.index.js
+++ assets/js/sript.index.js
@@ -5,10 +5,11 @@ function savePost() {
     const resume = document.getElementById("resume").value;
     const publisher = document.getElementById("publisher").value;
     const date = document.getElementById("date").value;
-    
+
     if (title && resume && publisher && date) {
         storePost(title, resume, publisher, date);
     }
+    showPosts();
 }
 
 function storePost(title, resume, publisher, date) {
@@ -19,5 +20,25 @@ function storePost(title, resume, publisher, date) {
         date
     };
 
-    posts.push(post);
+    posts.push(post)
+}
+
+function showPosts() {
+    let showContent = "";
+
+    posts.forEach((post, index) => {
+        showContent += `
+        <div class="post">
+        <h2>${post.title}</h2>
+        <p><strong>Resumo: </strong>${post.resume}</p>
+        <p><strong>Autor: </strong>${post.publisher}</p>
+        <p><strong>Data de publicação: </strong>${post.date}</p>
+
+        <button onclick="editPost(${index})">Editar</button>
+        <button onclick="removePost(${index})">Remover</button>
+        
+        </div>
+        `;
+    })
+    document.getElementById("list").innerHTML = showContent;
 }
